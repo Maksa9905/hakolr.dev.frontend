e3496d2b0f5117386f3b021f40f1b659f9eb46a8
hakolr <m.gayvoronskiy@flang.io>
2025-07-14 00:07:55 +0300

    fix: added prefetch for MainPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import MainPage from '@/screens/MainPage'
+import { prefetchMainPage } from './prefetch'
+import { dehydrate } from '@tanstack/react-query'
+import { HydrationBoundary } from '@tanstack/react-query'
 
-export default function Home() {
-  return <MainPage />
+export default async function Home() {
+  const queryClient = await prefetchMainPage()
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <MainPage />
+    </HydrationBoundary>
+  )
 }
