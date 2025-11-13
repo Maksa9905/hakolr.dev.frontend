eb0f1d0add35ba2a34f03d7c45f8a3ed27fd8036
hakolr <m.gayvoronskiy@flang.io>
2025-11-14 00:42:04 +0300

    fix: fixed api url

--- src/shared/api/ApiController.ts
+++ src/shared/api/ApiController.ts
@@ -1,9 +1,13 @@
 import axios, { AxiosRequestConfig } from 'axios'
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL
+  ? process.env.NEXT_PUBLIC_API_URL
+  : '/api'
+
 export class ApiController {
   static async call<T>(url: string, config: AxiosRequestConfig): Promise<T> {
     const response = await axios.request<T>({
-      url: `${process.env.NEXT_PUBLIC_API_URL}/api/${url}`,
+      url: `${BASE_URL}/${url}`,
       ...config,
     })
 
